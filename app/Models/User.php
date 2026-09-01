0c26550b799157905451a8b394eb72f174e7c724
Yasmin <yasmeenomar631@gmail.com>
2026-09-02 00:40:42 +0300

    Add telegram fields to fillable attribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'telegram_id', 'telegram_username', 'telegram_photo_url'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable implements FilamentUser
 {
