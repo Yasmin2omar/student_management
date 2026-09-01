1066fd762579f87dc85f0abc722ccef12a348984
Yasmin2omar <145024102+Yasmin2omar@users.noreply.github.com>
2026-09-01 22:02:34 +0300

    Update login.blade.php

--- resources/views/filament/pages/auth/login.blade.php
+++ resources/views/filament/pages/auth/login.blade.php
@@ -1,11 +1,6 @@
 <x-filament-panels::page.simple>
     <x-filament-panels::form wire:submit="authenticate">
         {{ $this->form }}
-
-        <x-filament-panels::form.actions
-            :actions="$this->getCachedFormActions()"
-            :full-width="$this->hasFullWidthFormActions()"
-        />
     </x-filament-panels::form>
 
     <div class="mt-6 flex flex-col items-center gap-2">
